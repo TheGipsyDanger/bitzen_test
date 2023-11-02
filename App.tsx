984520d93c3eb5c088e0977e16d700126a531af7
Renan O Henrique <henriquebasshvf@gmail.com>
2023-11-02 09:14:24 -0300

    feat: add a configs to use splash screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import {useEffect, useCallback, useState} from 'react';
+import {View} from 'react-native';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import store from './src/redux/store';
+import * as SplashScreen from 'expo-splash-screen';
 import Routes from './src/routes/Routes';
-
 import {
   useFonts,
   Poppins_400Regular,
@@ -11,21 +13,37 @@ import {
   Poppins_600SemiBold,
 } from '@expo-google-fonts/poppins';
 
+SplashScreen.preventAutoHideAsync();
+
 const App = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
   let [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_600SemiBold,
   });
 
-  if (!fontsLoaded) return <></>;
+  useEffect(() => {
+    if (fontsLoaded) return setAppIsReady(true);
+  }, [fontsLoaded]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) return <></>;
 
   return (
-    <NavigationContainer>
-      <Provider store={store}>
-        <Routes />
-      </Provider>
-    </NavigationContainer>
+    <View style={{flex: 1}} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Provider store={store}>
+          <Routes />
+        </Provider>
+      </NavigationContainer>
+    </View>
   );
 };
 
