d3878bb10bad64e00d810f50a6df051b75fd4852
Renan O Henrique <henriquebasshvf@gmail.com>
2023-11-04 15:54:07 -0300

    fix: create a methods to use a mask

--- src/utils/masks/index.ts
+++ src/utils/masks/index.ts
@@ -1,16 +1,39 @@
-import {ILoginForm} from '~/utils/interfaces';
+import {
+  ILoginForm,
+  IChangePasswordForm,
+  IRegisterPetForm,
+  IRegisterForm,
+  ISearchForm,
+  IResetPasswordForm,
+} from '~/utils/interfaces/formSchemaTypes';
 import {mask as masker} from 'remask';
 
-export type IMasks = keyof ILoginForm<string>;
+export type IMasks =
+  | keyof ILoginForm<string>
+  | keyof IChangePasswordForm<string>
+  | keyof IRegisterForm<string, boolean>
+  | keyof IRegisterPetForm<string>
+  | keyof ISearchForm<string>
+  | keyof IResetPasswordForm<string>;
 
 export type IMaskObj = {
-  [key in IMasks]: string;
+  [key in IMasks]: string | string[];
 };
 
 export const defineMask = (mask: IMasks) => {
   const options: IMaskObj = {
     email: '',
     password: '',
+    confirmedPassword: '',
+    phone: ['(99) 9999-9999', '(99) 99999-9999'],
+    image: '',
+    name: '',
+    color: '',
+    birth: '99/99/9999',
+    about: '',
+    query: '',
+    agreeTerms: '',
+    document: '999.999.999-99',
   };
   return options[mask] || '';
 };
